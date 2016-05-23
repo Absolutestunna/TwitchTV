78d1b20b6a958ee1f93bc4f6ac6b8356ca4332dd
Joshua Abu <joshujah007@gmail.com>
2016-05-22 20:52:26 -0400

    added search functionality

--- js/controllers.js
+++
@@ -1,9 +0,0 @@
-var artistControllers = angular.module('artistControllers', []);  //the '[]' will house the dependecies for the app
-
-myApp.controller('ListController', ['$scope', '$http', function($scope, $http){  //the scope is a super variable that allows us to pass things from js to application/template
-  $http.get('js/data.json').success(function(data){
-    $scope.artists = data;
-    $scope.artistOrder = 'name';
-
-  });
-}]);
